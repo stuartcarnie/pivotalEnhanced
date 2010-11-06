b2f3445ef8261cd6629037750a8996228357c62e
Stuart Carnie <stuart.carnie@gmail.com>
2010-11-05 20:54:58 -0700

    Initial markdown support working

--- PivotalEnhanced.safariextension/pivotalEnhanced.js
+++ PivotalEnhanced.safariextension/pivotalEnhanced.js
@@ -7,18 +7,50 @@ function main() {
         return;
     }
 
-    var script= document.createElement('script');
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://github.com/scarnie/pivotalEnhanced/raw/master/showdown.js'
+    head.appendChild(script)
+    
+    script= document.createElement('script');
     script.type= 'text/javascript';
-    script.innerText = checkElement.toString() + "\ncheckElement();"
+    script.innerText = engageMarkdown.toString() + "\nengageMarkdown();"
     head.appendChild(script);
-
-    function checkElement() {
+    
+    function engageMarkdown() {
+        console.log("engageMarkdown");
+        
+        if (typeof(Showdown) == 'undefined') {
+            console.log("running loop");
+            
+            var inter = window.setInterval(function() {
+                if (typeof(Showdown) != 'undefined') {
+                    window.clearInterval(inter);
+                    engageMarkdown();
+                }
+            }, 100);
+            return;
+        }
+        
         if (Element != undefined) {
             console.log("Element exists");
         }
 
         if (typeof(Element.cleanupText) == 'function') {
-            console.log("Element.cleanupText exists");
+            console.log("Engaging markdown");
+            Element.oldCleanupText = Element.cleanupText;
+            var converter = new Showdown.converter();
+            Element.cleanupText = function(text) {
+                if (text == null) {
+                    return "";
+                }
+                
+                if (text.match(/:MD$/)) {
+                    return converter.makeHtml(text.replace(/:MD$/, ''));
+                }
+
+                return Element.oldCleanupText(text);
+            }
         } else {
             console.log("Element.cleanupText does not exist");
         }
